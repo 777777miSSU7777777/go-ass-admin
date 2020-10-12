6a19c43757287e778a13420ef232cf6a7b18d84b
777777miSSU7777777 <6x7missu7x7@gmail.com>
2020-10-13 00:27:51 +0300

    refactor artist controller

--- backend/src/controller/data/artist/artist.controller.ts
+++ backend/src/controller/data/artist/artist.controller.ts
@@ -1,29 +1,96 @@
-import { Body, Controller, Delete, Get, Post, Put, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Post, Put, Res } from '@nestjs/common';
 import { ArtistService } from '@service';
 import { Artist } from '@model';
+import { Response } from 'express';
 
-@Controller()
+@Controller('/artist')
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
-  @Get('/artist')
-  async getArtists(): Promise<Artist[]> {
-    return await this.artistService.getArtists();
+  @Get()
+  async getArtists(@Res() res: Response) {
+    try {
+      const artists: Artist[] = await this.artistService.getArtists();
+      
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': artists,
+        'error': null,
+      });
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`Get Artists Error: ${e}`);
+    }
   }
 
-  @Post('/artist')
-  async newArtists(@Body() body: Artist[]): Promise<Artist[]> {
-    return await this.artistService.newArtists(body);
+  @Post()
+  async newArtists(@Body() body: Artist[], @Res() res: Response) {
+    try {
+      const newArtists = await this.artistService.newArtists(body);
+
+      res.status
+
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': newArtists,
+        'error': null,
+      });
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`New Artists Error: ${e}`);
+    }
   }
 
-  @Put('/artist')
-  async updateArtists(@Body() body: Artist[]): Promise<Artist[]> {
-    return await this.artistService.updateArtists(body);
+  @Put()
+  async updateArtists(@Body() body: Artist[], @Res() res: Response) {
+    try {
+      const updatedArtists = await this.artistService.updateArtists(body);
+
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': updatedArtists,
+        'error': null,
+      });
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`Update Artists Error: ${e}`);
+    }
   }
 
-  @Delete('/artist')
-  async deleteArtist(@Body() body: Artist[]): Promise<number[]> {
-    return await this.artistService.deleteArtists(body);
+  @Delete()
+  async deleteArtist(@Body() body: Artist[], @Res() res: Response) {
+    try {
+      const deletedRows = await this.artistService.deleteArtists(body);
+      
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': `Rows affected: ${deletedRows.reduce((acc: number = 0, cur: number) => acc + cur)}`,
+        'error': null,
+      });
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`Delete Artists Error: ${e}`);
+    }
   }
 }
 
