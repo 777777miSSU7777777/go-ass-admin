80ac0297aafd1d42321d12d758fde5d93858a375
777777miSSU7777777 <6x7missu7x7@gmail.com>
2020-10-13 15:56:45 +0300

    refactor track controller

--- backend/src/controller/data/track/track.controller.ts
+++ backend/src/controller/data/track/track.controller.ts
@@ -1,29 +1,94 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Post, Put, Res } from '@nestjs/common';
 import { TrackService } from '@service';
 import { Track } from '@model';
+import { Response } from 'express';
 
-@Controller()
+@Controller('/track')
 export class TrackController {
   constructor(private readonly trackService: TrackService) {}
 
-  @Get('/track')
-  async getTracks(): Promise<Track[]> {
-    return await this.trackService.getTracks();
+  @Get()
+  async getTracks(@Res() res: Response) {
+    try {
+      const tracks: Track[] = await this.trackService.getTracks();
+
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': tracks,
+        'error': null,
+      });
+    } catch(e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`Get Tracks Error: ${e}`);
+    }
   }
 
-  @Post('/track')
-  async newTracks(@Body() body: Track[]): Promise<Track[]> {
-    return await this.trackService.newTracks(body);
+  @Post()
+  async newTracks(@Body() body: Track[], @Res() res: Response) {
+    try {
+      const newTracks: Track[] = await this.trackService.newTracks(body);
+
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': newTracks,
+        'error': null,
+      });
+    } catch(e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`New Tracks Error: ${e}`);
+    }
   }
 
-  @Put('/track')
-  async updateTracks(@Body() body: Track[]): Promise<Track[]> {
-    return await this.trackService.updateTracks(body);
+  @Put()
+  async updateTracks(@Body() body: Track[], @Res() res: Response) {
+    try {
+      const updatedTracks: Track[] = await this.trackService.updateTracks(body);
+
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': updatedTracks,
+        'error': null,
+      });
+    } catch(e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`Update Tracks Error: ${e}`);
+    }
   }
 
-  @Delete('/track')
-  async deleteTracks(@Body() body: Track[]): Promise<number[]> {
-    return await this.trackService.deleteTracks(body);
+  @Delete()
+  async deleteTracks(@Body() body: Track[], @Res() res: Response) {
+    try {
+      const deletedRowsCount: number[] = await this.trackService.deleteTracks(body);
+
+      res.status(HttpStatus.OK).json({
+        'ok': true,
+        'data': `Affected rows count: ${deletedRowsCount.reduce((acc: number = 0, cur: number) => acc + cur)}`,
+        'error': null,
+      });
+    } catch(e) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        'ok': false,
+        'data': null,
+        'error': e,
+      });
+
+      console.error(`Delete Tracks Error: ${e}`);
+    }
   }
 }
 
